cb5f4e40b42e919608304e2e5a3749b0eb5fa038
Maddy <maddy@kitty.garden>
2025-01-21 20:36:10 +0100

    custom search engine time

---
+++ src/customSearchEngine/index.ts
@@ -0,0 +1,21 @@
+import { ExtensionWebExports } from "@moonlight-mod/types";
+
+// https://moonlight-mod.github.io/ext-dev/webpack/#patching
+export const patches: ExtensionWebExports["patches"] = [
+  {
+    find: "search-google",
+    replace: [
+      {
+        match: /"https:\/\/www\.google\.com\/search\?q="\.concat\(encodeURIComponent\((\i)\)\)/,
+        replacement: 'moonlight.getConfigOption("customSearchEngine","url").replace("%s",encodeURIComponent($1))'
+      },
+      {
+        match: /label:[^,]+,/,
+        replacement: 'label:moonlight.getConfigOption("customSearchEngine","label"),'
+      }
+    ]
+  }
+];
+
+// https://moonlight-mod.github.io/ext-dev/webpack/#webpack-module-insertion
+export const webpackModules: ExtensionWebExports["webpackModules"] = {};
