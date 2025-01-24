d10f14c8e9aac1cc265bc4ec42339c35c6a0a309
Maddy <maddy@kitty.garden>
2025-01-24 20:29:02 +0100

    fix a discord bug

---
+++ src/expandStatus/index.ts
@@ -0,0 +1,15 @@
+import { ExtensionWebExports } from "@moonlight-mod/types";
+
+// https://moonlight-mod.github.io/ext-dev/webpack/#patching
+export const patches: ExtensionWebExports["patches"] = [
+  {
+    find: 'action:"HOVER_CUSTOM_STATUS"',
+    replace: {
+      match: /onShowToolbar:\i/,
+      replacement: "$&=()=>{}"
+    }
+  }
+];
+
+// https://moonlight-mod.github.io/ext-dev/webpack/#webpack-module-insertion
+export const webpackModules: ExtensionWebExports["webpackModules"] = {};
