d87fc357c58a8faee4a54177786630777cdb4a50
Maddy <maddy@kitty.garden>
2025-02-04 17:02:24 +0100

    we had fun times together

--- src/expandStatus/index.ts
+++ src/expandStatus/index.ts
@@ -1,15 +1,7 @@
 import { ExtensionWebExports } from "@moonlight-mod/types";
 
 // https://moonlight-mod.github.io/ext-dev/webpack/#patching
-export const patches: ExtensionWebExports["patches"] = [
-  {
-    find: 'action:"HOVER_CUSTOM_STATUS"',
-    replace: {
-      match: /onShowToolbar:\i/,
-      replacement: "$&=()=>{}",
-    },
-  },
-];
+export const patches: ExtensionWebExports["patches"] = [];
 
 // https://moonlight-mod.github.io/ext-dev/webpack/#webpack-module-insertion
 export const webpackModules: ExtensionWebExports["webpackModules"] = {};
