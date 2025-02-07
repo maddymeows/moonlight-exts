54efa18fb482774fe1057c98b84d1c5ffabccfc9
Maddy <maddy@kitty.garden>
2025-02-07 18:52:04 +0100

    infinity

---
+++ src/showVoiceMemberCount/index.ts
@@ -0,0 +1,32 @@
+import { ExtensionWebExports } from "@moonlight-mod/types";
+
+// 876548
+// 648501
+
+// https://moonlight-mod.github.io/ext-dev/webpack/#patching
+export const patches: ExtensionWebExports["patches"] = [
+  {
+    find: "considerMaxStageVoiceUserLimit:",
+    replace: {
+      match: /return function\(\i\)\{/,
+      replacement: "$&return!0;",
+    },
+  },
+  {
+    find: "videoLimit:",
+    replace: {
+      match:
+        /\{className:(\i\(\)\(\i\.total,\{\[\i\.extraLong]:\i>=100}\)),children:(\i)\.toString\(\)\.padStart\(2,"0"\)}/,
+      replacement:
+        '{className:$1,children:$2>0?$2.toString().padStart(2,"0"):"\\u221e"}',
+    },
+  },
+];
+
+// https://moonlight-mod.github.io/ext-dev/webpack/#webpack-module-insertion
+export const webpackModules: ExtensionWebExports["webpackModules"] = {
+  replacer: {
+    dependencies: [{ ext: "commands", id: "commands" }],
+    entrypoint: true,
+  },
+};
