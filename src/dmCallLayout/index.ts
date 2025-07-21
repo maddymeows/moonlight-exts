c6931211dc8e5cc67b95e6031582a43e3f377784
Maddy <maddy@fabricpaws.net>
2025-07-22 01:37:51 +0200

    winning

---
+++ src/dmCallLayout/index.ts
@@ -0,0 +1,27 @@
+import { ExtensionWebExports } from "@moonlight-mod/types";
+
+// https://moonlight-mod.github.io/ext-dev/webpack/#patching
+export const patches: ExtensionWebExports["patches"] = [
+  {
+    find: '"displayName","ChannelRTCStore"',
+    replace: [
+      {
+        match: /getLayout\((\i)\)\{/,
+        replacement:
+          '$&if(spacepack.require("common_stores").ChannelStore.getChannel($1)?.isGuildVoice())return"normal";',
+      },
+    ],
+  },
+  {
+    find: '"Missing channel in Channel.renderChat"',
+    replace: [
+      {
+        match: /if\((\i)\.isGuildVocal\(\)\)return null;/,
+        replacement: "if($1.isGuildVocal()&&!$1.isGuildVoice())return null;",
+      },
+    ],
+  },
+];
+
+// https://moonlight-mod.github.io/ext-dev/webpack/#webpack-module-insertion
+export const webpackModules: ExtensionWebExports["webpackModules"] = {};
